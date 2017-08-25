826686b740c7b0d55385e26b4253ba2f46bd5274
bmyell <1812630796@qq.com>
2017-08-25 14:52:27 +0800

    change the Movie class construct function

--- media.py
+++ media.py
@@ -19,7 +19,7 @@ class Movie(Video):
     def __init__(self, movie_title, movie_duration, movie_storyline, poster_image, trailer_youtube):  # NOQA
         """Initialize title, durationm, storyline, poster_image_url,traler_youtube"""  # NOQA
         self.title = movie_title
-        Video(movie_title, movie_duration)
+        self.duration = movie_duration
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
